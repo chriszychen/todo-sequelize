6b35d41e1d76320115e2539e46b091b9504b94bc
Chris Chen <ok3201123@gmail.com>
2021-06-12 16:53:54 +0800

    feat: add user-related  routes and views

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcryptjs')
 const app = express()
 const PORT = 3000
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
@@ -15,5 +15,24 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
 
 app.listen(PORT, () => console.log(`App is running on http://localhost:${PORT}`))
