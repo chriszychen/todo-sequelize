4dcae6ffb6dce51de0ec2934a313133cbf93c608
Chris Chen <ok3201123@gmail.com>
2021-06-13 16:26:27 +0800

    feat: add new page and create function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,7 +3,23 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
-router.get('/todos/:id', (req, res) => {
+// render create page
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
+
+// CREATE: create a new todo to database
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const { name } = req.body
+
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
+})
+
+// READ: find a todo and render detail page
+router.get('/:id', (req, res) => {
   const id = req.params.id
   return Todo.findByPk(id)
     .then(todo => res.render('detail', { todo: todo.toJSON() }))
