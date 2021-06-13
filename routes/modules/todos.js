b853f0a80bd5e0e270f04da3934c34ed7ba95047
Chris Chen <ok3201123@gmail.com>
2021-06-13 16:56:43 +0800

    feat: add edit page and update function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -26,4 +26,28 @@ router.get('/:id', (req, res) => {
     .catch(error => console.log(error))
 })
 
+// render edit page
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => res.render('edit', { todo: todo.toJSON() }))
+    .catch(err => console.log(err))
+})
+
+// UPDATE: find a todo, edit, and save it
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(err => console.log(err))
+})
+
 module.exports = router
