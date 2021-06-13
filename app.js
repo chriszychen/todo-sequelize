53f65560c84b1bd6e1662aaa529fdf788216a6a9
Chris Chen <ok3201123@gmail.com>
2021-06-13 16:25:15 +0800

    feat: add user authenticator to routes and isAuthenticated to req.loacls

--- app.js
+++ app.js
@@ -22,6 +22,11 @@ app.use(session({
 
 usePassport(app)
 
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
 // --- set routes ---
 app.use(routes)
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,20 +5,20 @@ const bcrypt = require('bcryptjs')
 const db = require('../../models')
 const User = db.User
 
-router.get('/users/login', (req, res) => {
+router.get('/login', (req, res) => {
   res.render('login')
 })
 
-router.post('/users/login', passport.authenticate('local', {
+router.post('/login', passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/users/login'
 }))
 
-router.get('/users/register', (req, res) => {
+router.get('/register', (req, res) => {
   res.render('register')
 })
 
-router.post('/users/register', (req, res) => {
+router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
   User.findOne({ where: { email } }).then(user => {
     if (user) {
@@ -43,7 +43,7 @@ router.post('/users/register', (req, res) => {
   })
 })
 
-router.get('/users/logout', (req, res) => {
+router.get('/logout', (req, res) => {
   req.logout()
   res.redirect('/users/login')
 })
